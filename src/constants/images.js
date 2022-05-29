f508adebeac9c23c8ae79c4f3efe90b1056b2076
p3isman <pedro_eisman@outlook.com>
2022-05-29 17:14:23 +0200

    Change images format to webp and add some svgs

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,41 +1,33 @@
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-import admin_pacientes from '../assets/admin_pacientes.png';
-import api from '../assets/api.png';
+import admin_pacientes from '../assets/admin_pacientes.webp';
 import circle from '../assets/circle.svg';
-import css from '../assets/css.png';
-import email from '../assets/email.png';
+import css from '../assets/css.webp';
+import email from '../assets/email.webp';
 import express from '../assets/express.svg';
-import figma from '../assets/figma.png';
-import find_the_spy from '../assets/find_the_spy.png';
-import flutter from '../assets/flutter.png';
-import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
-import html from '../assets/html.png';
+import figma from '../assets/figma.webp';
+import find_the_spy from '../assets/find_the_spy.webp';
+import flutter from '../assets/flutter.svg';
+import git from '../assets/git.webp';
+import graphql from '../assets/graphql.webp';
+import html from '../assets/html.webp';
 import javascript from '../assets/javascript.svg';
-import minimal_reddit from '../assets/minimal_reddit.png';
-import mobile from '../assets/mobile.png';
-import mobile_dev from '../assets/mobile_dev.jpg';
-import movies from '../assets/movies.png';
-import node from '../assets/node.png';
-import open_emt from '../assets/open_emt.png';
-import portfolio from '../assets/portfolio.jpg';
-import python from '../assets/python.png';
-import qr_scanner from '../assets/qr_scanner.png';
-import quotia from '../assets/quotia.png';
+import minimal_reddit from '../assets/minimal_reddit.webp';
+import mobile from '../assets/mobile.webp';
+import mobile_dev from '../assets/mobile_dev.webp';
+import movies from '../assets/movies.webp';
+import node from '../assets/node.svg';
+import open_emt from '../assets/open_emt.webp';
+import portfolio from '../assets/portfolio.webp';
+import qr_scanner from '../assets/qr_scanner.webp';
+import quotia from '../assets/quotia.webp';
 import react from '../assets/react.svg';
-import redux from '../assets/redux.png';
-import sass from '../assets/sass.png';
+import redux from '../assets/redux.svg';
+import sass from '../assets/sass.svg';
 import typescript from '../assets/typescript.svg';
-import vue from '../assets/vue.png';
-import web_dev from '../assets/web_dev.png';
+import web_dev from '../assets/web_dev.webp';
 
 const images = {
   email,
   mobile,
-  api,
   css,
   figma,
   flutter,
@@ -45,16 +37,10 @@ const images = {
   javascript,
   node,
   express,
-  python,
   react,
   redux,
   sass,
   typescript,
-  vue,
-  about01,
-  about02,
-  about03,
-  about04,
   circle,
   web_dev,
   mobile_dev,
