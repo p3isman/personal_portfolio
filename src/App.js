b446f48698588b9bbe90a358cd33ac14b70fbd2b
p3isman <pedro_eisman@outlook.com>
2022-06-09 20:55:07 +0200

    Wrap app with wrapper and context provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
+import AppContext from './context/AppContextProvider';
 import './App.scss';
-import { Navbar } from './components';
-import { About, Footer, Header, Skills, Work } from './container/index';
+import { AppWrapper } from './wrapper';
 
 const App = () => {
   return (
-    <div className='app app__relative'>
-      <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Footer />
-    </div>
+    <AppContext>
+      <AppWrapper />
+    </AppContext>
   );
 };
 
