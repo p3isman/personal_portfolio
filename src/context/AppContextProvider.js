efadc422813d10e868cfb2cbc1f723d626c77cbe
p3isman <pedro_eisman@outlook.com>
2022-06-10 11:07:21 +0200

    Load saved theme or system theme instead

--- src/context/AppContextProvider.js
+++ src/context/AppContextProvider.js
@@ -1,13 +1,21 @@
-import { createContext, useRef, useState } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
 
 export const AppContext = createContext(null);
 
 const AppContextProvider = ({ children }) => {
-  const storedTheme = window.localStorage.getItem('theme');
-  const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
-  const [theme, setTheme] = useState(
-    storedTheme ?? prefersDarkTheme.matches ? 'dark' : 'light'
-  );
+  const systemTheme =
+    matchMedia && matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  const [theme, setTheme] = useState(systemTheme);
+
+  useEffect(() => {
+    const forcedTheme = localStorage.getItem('theme');
+    if (forcedTheme) {
+      setTheme(forcedTheme);
+    }
+  }, []);
+
   const inputRef = useRef(null);
 
   const onChangeTheme = () => {
