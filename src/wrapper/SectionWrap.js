421e6d1b2fe3336e1ba48917698729509270db39
p3isman <pedro_eisman@outlook.com>
2022-06-14 08:01:06 +0200

    Remove navigation dots from layout

--- src/wrapper/SectionWrap.js
+++ src/wrapper/SectionWrap.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationDots } from '../components';
 
 const SectionWrap = (Component, idName, classNames) => () => {
   return (
@@ -7,7 +6,6 @@ const SectionWrap = (Component, idName, classNames) => () => {
       <div className='app__wrapper app__flex'>
         <Component />
       </div>
-      <NavigationDots active={idName} />
     </div>
   );
 };
