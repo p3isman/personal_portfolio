81882b122a0a9d4e0ed0d270309173913d849418
p3isman <pedro_eisman@outlook.com>
2022-06-30 15:04:41 +0200

    Fetch abouts from Sanity

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,23 +1,18 @@
 import { motion } from 'framer-motion'
-import images from '../../constants/images'
+import { useEffect, useState } from 'react'
+import { fetchAbouts, urlFor } from '../../api/sanityClient'
 import { SectionWrap } from '../../wrapper'
 import './About.scss'
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description:
-      'I enjoy building beautifully designed websites and functional web applications.',
-    imgUrl: images.web_dev
-  },
-  {
-    title: 'Mobile Development',
-    description: 'I like giving birth to my ideas by making mobile apps.',
-    imgUrl: images.mobile_dev
-  }
-]
-
 const About = () => {
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    fetchAbouts().then((data) => {
+      setAbouts(data)
+    })
+  }, [])
+
   return (
     <div>
       <h2 className='head-text'>
@@ -35,14 +30,14 @@ const About = () => {
       </p>
 
       <div className='app__profiles'>
-        {abouts.map((about, index) => (
+        {abouts.map((about) => (
           <motion.div
             animate={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.3 }}
             className='app__profile-item'
-            key={about.title + index}>
-            <img src={about.imgUrl} alt={about.title} />
+            key={about.title}>
+            <img src={urlFor(about.imgUrl).size(600, 600)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
