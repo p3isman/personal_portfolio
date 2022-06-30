6d255c05e8d06e14f29c80d3eb88c9acd623315c
p3isman <pedro_eisman@outlook.com>
2022-06-30 15:03:58 +0200

    Add cookie consent banner with Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import AppContext from './context/AppContextProvider'
 import './App.scss'
 import { AppWrapper } from './wrapper'
+import CookieConsent, { Cookies } from 'react-cookie-consent'
+import ReactGA from 'react-ga'
 
 const App = () => {
+  const handleAcceptCookie = () => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID, {
+      debug: true
+    })
+  }
+
+  const handleDeclineCookie = () => {
+    Cookies.remove('_ga')
+    Cookies.remove('_gat')
+    Cookies.remove('_god')
+  }
+
   return (
     <AppContext>
       <AppWrapper />
+      <CookieConsent
+        enableDeclineButton
+        location='bottom'
+        buttonText='Accept'
+        declineButtonText='Decline'
+        onAccept={handleAcceptCookie}
+        onDecline={handleDeclineCookie}>
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </AppContext>
   )
 }
