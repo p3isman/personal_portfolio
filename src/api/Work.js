2d5778cf358e9e082b7e6feccbddef4265dc6069
p3isman <pedro_eisman@outlook.com>
2022-06-30 15:04:26 +0200

    Centralize API calls in sanityClient.js

--- src/api/Work.js
+++
@@ -1,8 +0,0 @@
-import { client } from '../api/sanityClient'
-
-export const fetchWorks = async () => {
-  const query = '*[_type=="works"] | order(order asc)'
-
-  const data = await client.fetch(query)
-  return data
-}
