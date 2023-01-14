433ee5b6b8713ed286252113f64566f6a5e7e807
p3isman <pedro_eisman@outlook.com>
2023-01-15 00:27:04 +0100

    Use Plausible instead of Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { useEffect } from 'react';
-import CookieConsent, { getCookieConsentValue } from 'react-cookie-consent';
-import ReactGA from 'react-ga4';
 import ThemeProvider from 'context/ThemeProvider';
 import { AppWrapper } from 'wrapper';
 import './App.scss';
 
 const App = () => {
-  const handleAcceptCookie = () => {
-    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-  };
-
-  useEffect(() => {
-    if (getCookieConsentValue()) {
-      handleAcceptCookie();
-    }
-  }, []);
-
   return (
     <ThemeProvider>
       <AppWrapper />
-      <CookieConsent
-        enableDeclineButton
-        containerClasses='cookie-banner'
-        buttonClasses='cookie-banner__button'
-        declineButtonClasses='cookies-banner__button decline'
-        location='bottom'
-        buttonText='Accept'
-        declineButtonText='Decline'
-        onAccept={handleAcceptCookie}>
-        This website uses cookies to enhance the user experience.
-      </CookieConsent>
     </ThemeProvider>
   );
 };
